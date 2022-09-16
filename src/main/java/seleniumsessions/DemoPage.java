315a3f397b16abc1dd065b4bb99e93cee78022ee
amitghute7 <amitghute7@gmailcom>
2022-09-16 13:29:52 +0530

    demopage is added and one method updated

---
+++ src/main/java/seleniumsessions/DemoPage.java
@@ -0,0 +1,14 @@
+package seleniumsessions;
+
+public class DemoPage {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("this is my demopage");
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
