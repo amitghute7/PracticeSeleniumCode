fdb442a007e81ffa9535be57c16e3be46ce709ae
Amit Ghute <113516429+amitghute7@users.noreply.github.com>
2022-09-16 18:26:10 +0530

    Create OpenCart.java

---
+++ src/main/java/seleniumsessions/OpenCart.java
@@ -0,0 +1,12 @@
+package seleniumsessions;
+
+public class OpenCart {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("this my open cart java code");
+
+	}
+
+}
