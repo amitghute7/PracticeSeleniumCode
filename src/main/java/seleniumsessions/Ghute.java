5746d8a4f633aebcca3e97aa89447163297e4476
Amit Ghute <113516429+amitghute7@users.noreply.github.com>
2022-09-16 18:32:47 +0530

    Create Ghute.java

---
+++ src/main/java/seleniumsessions/Ghute.java
@@ -0,0 +1,13 @@
+package seleniumsessions;
+
+public class Ghute {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("this my ghute class  java code");
+
+	}
+
+}
+
